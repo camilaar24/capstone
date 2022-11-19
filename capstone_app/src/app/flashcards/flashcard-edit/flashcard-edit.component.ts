b1ad86cdc65b4a97dcbd9e341dfbd9259c87bdaa
CAMILA ROMERO <camilaar@byui.edu>
2022-11-19 12:36:30 -0500

    flashcard_component

--- capstone_app/src/app/flashcards/flashcard-edit/flashcard-edit.component.ts
+++ capstone_app/src/app/flashcards/flashcard-edit/flashcard-edit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,EventEmitter,OnInit,Output,ViewChild, } from '@angular/core';
+import { Flashcard } from '../flashcard.model';
+import { FlashcardService } from '../flashcard.service';
 
 @Component({
   selector: 'app-flashcard-edit',
   templateUrl: './flashcard-edit.component.html',
   styleUrls: ['./flashcard-edit.component.css']
 })
-export class FlashcardEditComponent {
+export class FlashcardEditComponent implements OnInit {
+  @ViewChild('question')
+  question!: ElementRef;
+  @ViewChild('answer')
+  answer!: ElementRef;
 
-}
+  constructor(private flaschardService: FlashcardService) {}
+
+  ngOnInit(): void {}
+    
+    onSendFlashcard() {
+      const question = this.question.nativeElement.value;
+      const answer = this.answer.nativeElement.value;
+      const flashcard = new Flashcard ('1', question, answer);
+      this.flaschardService.addFlashcard(flashcard);
+    }
+
+    onClear() {
+      this.question.nativeElement.value='';
+      this.answer.nativeElement.value='';
+    }
+  }
+
